2bc228b68902b4dbb3798687f1184241b4d50ca3
Don Kallail <dkallail@gail.com>
2023-03-17 15:40:11 -0400

    moved math logic to its own file. updated css for better printing

--- app.py
+++ app.py
@@ -1,46 +1,7 @@
-from flask import Flask, render_template
-import random
+from flask import Flask, render_template, request
+from math_generator import *
 
 app = Flask(__name__)
-
-
-def generate_addition_problem():
-    num1 = random.randint(1, 100)
-    num2 = random.randint(1, 100)
-    problem = {
-        "operand1": num1,
-        "operand2": num2,
-        "operator": "+",
-        "answer": num1 + num2
-    }
-    return problem
-
-def generate_multiplication_problem():
-    num1 = random.randint(1, 12)
-    num2 = random.randint(1, 12)
-    problem = {
-        "operand1": num1,
-        "operand2": num2,
-        "operator": "x",
-        "answer": num1 * num2
-    }
-    return problem
-
-def generate_division_problem():
-    index = 0
-    while index < 19:
-        num1 = random.randint(1, 12)
-        num2 = random.randint(1, 12)
-        if (num1%num2) % 2 == 0 and num2 >= num1:
-            problem = {
-                "operand1": num1,
-                "operand2": num2,
-                "operator": "÷",
-                "answer": num1 / num2
-            }
-            index += 1
-            return problem
-
 
 
 
@@ -59,7 +20,7 @@ def multiplication():
     problems = [generate_multiplication_problem() for _ in range(20)]
     return render_template('problems.html', problems=problems, enumerate=enumerate)
 
-@app.route('/division')
+@app.route('/division', methods=['GET'])
 def division():
     problems = [generate_division_problem() for _ in range(20)]
     return render_template('problems.html', problems=problems, enumerate=enumerate)
---
+++ math_generator.py
@@ -0,0 +1,43 @@
+import random
+import time
+
+# Random number with system time
+random.seed(time.time())
+
+
+def generate_addition_problem():
+    num1 = random.randint(1, 100)
+    num2 = random.randint(1, 100)
+    problem = {
+        "operand1": num1,
+        "operand2": num2,
+        "operator": "+",
+        "answer": num1 + num2
+    }
+    return problem
+
+def generate_multiplication_problem():
+    num1 = random.randint(1, 12)
+    num2 = random.randint(1, 12)
+    problem = {
+        "operand1": num1,
+        "operand2": num2,
+        "operator": "x",
+        "answer": num1 * num2
+    }
+    return problem
+
+def generate_division_problem():
+    index = 0
+    while index < 19:
+        num1 = random.randint(1, 144)
+        num2 = random.randint(1, 12)
+        if num1 >= num2 and (num1%num2) == 0 and ((num1/num2) <= 12):
+            problem = {
+                "operand1": num1,
+                "operand2": num2,
+                "operator": "÷",
+                "answer": num1 / num2
+            }
+            index += 1
+            return problem
